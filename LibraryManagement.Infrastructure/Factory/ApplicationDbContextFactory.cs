f30eeeda5f994ed188efbd3fe1c26a30e33b7520
adamw <adamwroblewski.1993@gmail.com>
2025-07-15 14:58:48 +0200

    Added ApplicationDbContextFactory

---
+++ LibraryManagement.Infrastructure/Factory/ApplicationDbContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using LibraryManagement.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace LibraryManagement.Infrastructure
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            // Adjust the path as needed for your solution structure
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables()
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
